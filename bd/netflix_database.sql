36b994d57ebf47b0f1ddc423496e97a38bd45a66
TSalvatellaP <tsalvatellap@gmail.com>
2025-02-11 11:10:49 +0100

    update db

--- bd/netflix_database.sql
+++ bd/netflix_database.sql
@@ -23,3 +23,23 @@ CREATE TABLE actors (
     country VARCHAR(45) not null,
     birthday date
 );
+
+insert into movies (title, genre, image, category, year) values
+("Pulp Fiction", "Crimen", "https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg", "Top 10",1994 ),
+("La vita è bella", "Comedia", "https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg", "Top 10",1996 ),
+("Forrest Gump", "Comedia", "https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg", "Top 10",1994 );
+
+insert into users (user, password, name,email, plan_details) values
+("laura_dev", "laura","Laura","laura@gmail.com", "Standard"),
+("maria_dev", "maria","Maria","maria@gmail.com", "Standard"),
+("ester_dev", "ester","Ester","ester@gmail.com", "Standard");
+
+insert into actors (name, lastname, country, birthday) values
+("Tom", "Hanks", "Estados Unidos", "1956-07-09"),
+("Roberto", "Benigni", "Italia", "1952-10-27"),
+("John", "Travolta", "Estados Unidos", "1954-02-18");
+
+select * from 
+
+
+
